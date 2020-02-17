e6a96c78dab87b120d61e71ee56f2817e6342243
achyuth reddy <ncschyuth@gmail.com>
2020-02-17 11:06:33 +0530

    deletecommit

---
+++ helloworld.js
@@ -0,0 +1,12 @@
+console.log('hello world')
+console.log('hello world'>'achyuth')
+console.log(4>2 ? 1:2)
+console.log('5'-1)
+console.log(`hey 'achyuth'`)
+console.log('hey \'achyuth\'')
+let actual = '100'
+let bonus = 20
+let sum = actual -bonus
+console.log(sum)
+a = false
+console.log(a+1)
